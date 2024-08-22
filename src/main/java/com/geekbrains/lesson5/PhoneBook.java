be97b9d244ad0f2094a71be308d9b9a19b028e45
AlexeyIvanov1981 <118156280+AlexeyIvanov1981@users.noreply.github.com>
2024-08-22 09:24:02 +0300

    задание 5 рефакторинг2

--- src/main/java/com/geekbrains/lesson5/PhoneBook.java
+++ src/main/java/com/geekbrains/lesson5/PhoneBook.java
@@ -1,20 +1,28 @@
 package com.geekbrains.lesson5;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PhoneBook {
 
-    private Map<String, String> phoneBook = new HashMap<>();
+    private Map<String, List<String>> phoneBook = new HashMap<>();
 
     public void add(String userName, String phoneNumber) {
-        phoneBook.put(phoneNumber, userName);
+        List<String> phones = new ArrayList<>();
+        if (phoneBook.containsKey(userName)) {
+            phoneBook.get(userName).add(phoneNumber);
+        } else {
+            phones.add(phoneNumber);
+            phoneBook.put(userName, phones);
+        }
     }
 
     public void get(String userName) {
-        for (Map.Entry<String, String> map : phoneBook.entrySet()) {
-            if (userName.equals(map.getValue())) {
-                System.out.println(map.getKey());
+        for (Map.Entry<String, List<String>> map : phoneBook.entrySet()) {
+            if (userName.equals(map.getKey())) {
+                System.out.println(map.getValue());
             }
         }
     }
